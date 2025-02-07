a73e2979c86f9999526a303d25405290e37c53d1
poga07 <chris@168money.com.tw>
2025-02-07 13:08:37 +0800

    Initial Commit

---
+++ SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by 黃柏嘉 on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
